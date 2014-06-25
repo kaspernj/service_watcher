39ea234759bf3a3263d77721de411b1c89e1a8e3
kaspernj <k@spernj.org>
2014-06-25 14:29:14 +0200

    Update plugins and reporter plugins when deploing. Restart apache after deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,10 +20,10 @@ set :repo_url, 'https://github.com/kaspernj/service_watcher.git'
 # set :log_level, :debug
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -36,15 +36,12 @@ set :repo_url, 'https://github.com/kaspernj/service_watcher.git'
 
 # require 'whenever/capistrano'
 
-set :linked_files, %w{config/database.yml}
-
 namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+    on roles(:web), in: :sequence, wait: 5 do
+      execute :sudo, "apache2ctl graceful"
     end
   end
 
@@ -53,9 +50,15 @@ namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          puts "Updating plugins."
+          execute :rake, 'update_plugins'
+          
+          puts "Updating reporter plugins."
+          execute :rake, 'update_reporter_plugins'
+        end
+      end
     end
   end
 
