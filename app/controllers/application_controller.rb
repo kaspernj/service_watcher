87077292394288f0f879f6382988230b61198fc7
kaspernj <k@spernj.org>
2014-06-26 08:32:28 +0200

    Force sign in by redirecting to the new user session page unless there already or signed in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :force_sign_in
   
 private
   
@@ -17,5 +18,9 @@ private
     else
       authorize! action_name.to_sym, clazz
     end
+  end
+  
+  def force_sign_in
+    redirect_to new_user_session_path if !signed_in? && controller_name != "user_sessions"
   end
 end
